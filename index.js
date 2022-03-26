533e923385d3f991b53ae2203255f8d6cf409496
sergeypevniy <102419179+sergeypevniy@users.noreply.github.com>
2022-03-26 15:03:03 +0200

    Create index.js
    test

---
+++ index.js
@@ -0,0 +1,18 @@
+<html>
+<head>
+<script type="text/javascript">
+function displayDate()
+{
+document.getElementById("demo").innerHTML=Date();
+}
+</script>
+</head>
+<body>
+
+<h1>Моя Первая Веб Страница</h1>
+<p id="demo">Это параграф.</p>
+
+<button type="button" onclick="displayDate()">Показать Дату</button>
+
+</body>
+</html>
